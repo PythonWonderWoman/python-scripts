bf6a8f05fd40db2e79b00f369b74a5eb7fceb88a
PythonWonderWoman <python.wonder.woman@gmail.com>
2021-12-13 21:22:21 -0800

    nauka z ksiazki

---
+++ r1_01.py
@@ -0,0 +1,9 @@
+# program r1_01.py
+
+import sys
+
+dane = sys.implementation
+print("To jest moj pierwszy program w jezyku Python")
+print("IDLE z Python w systemie Windows:")
+print("================")
+print(dane)
