3d1958ab5cb94357f80a8d1c0f8cfbf0eabb2ac2
PythonWonderWoman <python.wonder.woman@gmail.com>
2021-12-13 21:23:40 -0800

    .

--- en_def_func_and_random_excercise.py
+++ en_def_func_and_random_excercise.py
@@ -3,22 +3,38 @@
 
     author: python.wonder.woman@gmail.com
     wersja EN
-    this is a simple script which is using random module and def function
+    this is a simple script which is using:
+                random module,
+                def functions,
+                regex
 
 """
 
 import random
+import re
 
 def hello(login):
     email = input("What is your email address?: ")
+
+    def isEmail():
+        emailRegex = re.compile(r'\w+[.|_]\w+@\w+.\w+')
+        print(emailRegex.findall(email))
+        print("\n")
+        if :
+            print("")
+
+    isEmail()
+
     set_pass = input("Enter 3 uppercase letters: ")
     set_pass2 = input("Enter 3 spacial characters: ")
     passwd = random.randint(1, 73)
 
-    print("Hello there!: " + login)
+    print("Hello there user: " + login + ".")
     print("Your email addrress is: " + email)
     print("Your login to the system is: " + login)
     print("Your password is: " + set_pass + str(passwd)*2 + set_pass2 + str(passwd))
     print("Your can reset your password anytime using 'Forgot password button'.")
+
+
 
 hello('python.wonder.woman@gmail.com')
